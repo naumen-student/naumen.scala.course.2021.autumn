5d292e2f26361ea5c5cea62c41f409e930e7c7cf
EvelinaYametova <57861063+EvelinaYametova@users.noreply.github.com>
2021-09-18 03:46:32 +0500

    Delete homeworks/homework_1/src/main/scala/example directory

--- homeworks/homework_1/src/main/scala/example/Main.scala
+++
@@ -1,9 +0,0 @@
-package example
-
-object Main extends App {
-  var getString = (g: String, name: String) => g.concat(" Scala! This is ").concat(name)
-  var greetings = List("Hello", "Hola", "Guten tag")
-  var name = "Evelina Yametova"
-  greetings.foreach(g => println(getString(g,name)))
-  greetings.foreach(g => println(getString(g,name.reverse)))
-}
