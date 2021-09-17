429fcafc31ab1685875317dc137b1f58fbc6131e
AndrewYaroshenko <90884335+AndrewYaroshenko@users.noreply.github.com>
2021-09-17 09:21:32 +0500

    Add files via upload

---
+++ homeworks/homework_1/task_3/src/main/scala/HelloWorld.scala
@@ -0,0 +1,34 @@
+object HelloWorld extends App {
+    val s = "Andrew"
+    val nach = "Hello Scala!"
+    val ser = "This is "
+    val zam1 = "Hola!"
+    val zam2 = "Guten tag!"
+    val name = "Andrew Yaroshenko -> "
+    val _1 = "A"
+    val _2 = "n"
+    val _3 = "d"
+    val _4 = "r"
+    val _5 = "e"
+    val _6 = "w"
+    val _1_ = "Y"
+    val _2_ = "r"
+    val _3_ = "o"
+    val _4_ = "s"
+    val _5_ = "h"
+    val _6_ = "e"
+    val _7_ = "n"
+    val _8_ = "k"
+    val _9_ = "o"
+    val fullName = List(_1, _2, _3, _4, _5, _6)
+    println(nach + ser + fullName)
+    println(nach + ser + _9_ + _8_ + _7_ + _6_ + _5_ + _4_ + _3_ + _2_ + _1_ + " " + _6 + _5 + _4 + _3 + _2 + _1 + "! ")
+    println("Hello " + s)
+    println(nach + ser + s)
+    println("Hello Scala! This is Andrew!")
+    println(zam1 + ser + s)
+
+
+
+
+}
