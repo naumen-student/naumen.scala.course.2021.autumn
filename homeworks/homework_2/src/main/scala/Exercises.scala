9076b9f22f9ae1f508fcef3bfa502f91950eaff2
Vlad Atkishkin <vladatkishkin@vk.com>
2021-09-23 16:54:15 +0500

    task 3 && task 4 -> done

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -1,13 +1,14 @@
 import scala.collection.mutable.ListBuffer
+import scala.math._
 
 object Exercises {
 
-    // Возвращает последовательность чисел кратных 3 или 7
+    /* ЗАДАНИЕ 0 */
+    // Функция, которая возвращает последовательность чисел кратных 3 или 7
     def divBy3Or7(
         start: Int,
         stop: Int
-    ):
-    Seq[Int] = {
+    ): Seq[Int] = {
         for {
             i <- start to stop
             if i % 3 == 0 ||
@@ -24,8 +25,7 @@ object Exercises {
     def sumOfDivBy3Or5(
         start: Int,
         finish: Int
-    ):
-    Long = {
+    ): Long = {
         val range = start to finish
 
         range.filter(
@@ -41,7 +41,9 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = {
+    def primeFactor(
+        number: Int
+    ): Seq[Int] = {
         var n = number
         var divisor = 2
         val factors = new ListBuffer[Int]
@@ -68,20 +70,44 @@ object Exercises {
     Функция sumCosines должна вычислять сумму косинусов углов между парами векторов cosBetween(leftVec0, leftVec1) + cosBetween(rightVec0, rightVec1).*/
     /*Реализовать юнит-тесты в src/test/scala для функций sumScalars и sumCosines*/
     case class Vector2D(x: Double, y: Double)
-    def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
-    def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
-    def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
-    def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
-        sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
-    def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
-        sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
 
+    def abs(vec: Vector2D): Double =  {
+        sqrt(vec.x * vec.x + vec.y * vec.y)
+    }
 
+    def scalar(vec0: Vector2D, vec1: Vector2D): Double = {
+        vec0.x * vec1.x + vec0.y * vec1.y
+    }
+
+    def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = {
+        scalar(vec0, vec1) / abs(vec0) / abs(vec1)
+    }
+
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D,
+           func: (Vector2D, Vector2D) => Double,
+           rightVec0: Vector2D, rightVec1: Vector2D
+    ): Double = {
+        func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
+    }
+
+    def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D,
+                   rightVec0: Vector2D, rightVec1: Vector2D
+    ): Double = {
+        sumByFunc(
+            leftVec0, leftVec1, scalar,
+            rightVec0, rightVec1
+        )
+    }
+
+    def sumCosines(
+        leftVec0: Vector2D, leftVec1: Vector2D,
+        rightVec0: Vector2D, rightVec1: Vector2D
+    ): Double = {
+        sumByFunc(
+            leftVec0, leftVec1, cosBetween,
+            rightVec0, rightVec1
+        )
+    }
 
     /*ЗАДАНИЕ IV*/
     /*Дано: коллекция металлических шариков balls, где каждый элемент представлен в виде (Name: String -> (radius: Int, density: Double).
@@ -92,16 +118,32 @@ object Exercises {
     В качестве значения числа "Пи" можно использовать java.lang.Math.PI
     */
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    val balls: Map[String, (Int, Double)] =
-        Map(
-            "Aluminum" -> (3,   2.6889), "Tungsten" ->  (2,   19.35), "Graphite" ->  (12,  2.1),   "Iron" ->      (3,   7.874),
-            "Gold" ->     (2,   19.32),  "Potassium" -> (14,  0.862), "Calcium" ->   (8,   1.55),  "Cobalt" ->    (4,   8.90),
-            "Lithium" ->  (12,  0.534),  "Magnesium" -> (10,  1.738), "Copper" ->    (3,   8.96),  "Sodium" ->    (5,   0.971),
-            "Nickel" ->   (2,   8.91),   "Tin" ->       (1,   7.29),  "Platinum" ->  (1,   21.45), "Plutonium" -> (3,   19.25),
-            "Lead" ->     (2,   11.336), "Titanium" ->  (2,   10.50), "Silver" ->    (4,   4.505), "Uranium" ->   (2,   19.04),
-            "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
+    val balls: Map[String, (Int, Double)] = Map(
+            "Aluminum" -> (3, 2.6889), "Tungsten" -> (2, 19.35),
+            "Graphite" -> (12, 2.1), "Iron" -> (3, 7.874),
+            "Gold" -> (2, 19.32), "Potassium" -> (14, 0.862),
+            "Calcium" -> (8, 1.55), "Cobalt" -> (4, 8.90),
+            "Lithium" -> (12, 0.534), "Magnesium" -> (10, 1.738),
+            "Copper" -> (3, 8.96), "Sodium" -> (5, 0.971),
+            "Nickel" -> (2, 8.91), "Tin" -> (1, 7.29),
+            "Platinum" -> (1, 21.45), "Plutonium" -> (3, 19.25),
+            "Lead" -> (2, 11.336), "Titanium" -> (2, 10.50),
+            "Silver" -> (4, 4.505), "Uranium" -> (2, 19.04),
+            "Chrome" -> (3, 7.18), "Cesium" -> (7, 1.873),
+            "Zirconium" -> (3, 6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(
+        ballsArray: Map[String, (Int, Double)] = balls
+    ): Seq[String] = {
+        val coeff = 4 / 3
 
+        ballsArray.map(
+            ball => (
+              ball._1,
+              coeff * Pi * pow(ball._2._1, 3) * ball._2._2
+        )).toList
+          .sortWith(_._2 < _._2)
+          .map(_._1)
+    }
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,8 +1,9 @@
+import Exercises.Vector2D
 import utest._
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-    val tests: Tests = Tests{
+    val tests: Tests = Tests {
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
@@ -40,5 +41,48 @@ object Test extends TestSuite{
             assert(Exercises.primeFactor(80) == Seq(2, 5))
             assert(Exercises.primeFactor(98) == Seq(2, 7))
         }
+
+       'test_sumScalar - {
+         assert(Exercises.sumScalars(
+           Vector2D(1, 3), Vector2D(-1, 4),
+           Vector2D(1, 4), Vector2D(2, 3)) == 25
+         )
+
+         assert(Exercises.sumScalars(
+           Vector2D(3, 2), Vector2D(1, 0),
+           Vector2D(2, 4), Vector2D(5, 7)) == 41
+         )
+       }
+
+      'test_sumCosines - {
+        assert(Exercises.sumCosines(
+          Vector2D(1, 3), Vector2D(-1, 4),
+          Vector2D(1, 4), Vector2D(2, 3)) == 1.785403399326945
+        )
+
+        assert(Exercises.sumCosines(
+          Vector2D(1, 0), Vector2D(1, 0),
+          Vector2D(2, 0), Vector2D(0, 2)) == 1
+        )
+      }
+
+      'sortByHeavyweight - {
+        assert(
+          Exercises.sortByHeavyweight(Map(
+            "Vladek" -> (1, 3.14),
+            "Vladislav" -> (2, 2.71)
+          )) == List("Vladek", "Vladislav")
+        )
+
+        assert(
+          Exercises.sortByHeavyweight(
+          Exercises.balls
+        ) == List("Tin", "Platinum", "Nickel",
+          "Aluminum", "Titanium", "Lead",
+          "Sodium", "Uranium", "Gold", "Tungsten",
+          "Zirconium", "Chrome", "Iron", "Copper",
+          "Silver", "Plutonium", "Cobalt", "Cesium",
+          "Calcium", "Lithium", "Magnesium", "Potassium", "Graphite"))
+      }
     }
 }
