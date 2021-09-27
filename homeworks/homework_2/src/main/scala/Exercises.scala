b2bc14813c6348757964afe9fb45b725fcf5840e
Max Ratkov <32338709+mattgroy@users.noreply.github.com>
2021-09-27 20:01:49 +0500

    homework_2 - Ратков Максим

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object Exercises {
 
     /*ПРИМЕР*/
@@ -16,7 +18,7 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = (iFrom to iTo).filter(e => e % 3 == 0 || e % 5 == 0).sum
 
 
 
@@ -25,8 +27,21 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
-
+    def primeFactor(number: Int): Seq[Int] = {
+        val result = mutable.ArrayBuffer[Int]()
+        var prime = 2
+        var div = number.abs
+        while (div >= prime * prime) {
+            if (div % prime != 0)
+                prime += 1
+            else {
+                div /= prime
+                if (prime > 1 && (result.isEmpty || result.last != prime)) result += prime
+            }
+        }
+        if (div > 1 && (result.isEmpty || result.last != div)) result += div
+        result
+    }
 
 
     /*ЗАДАНИЕ III*/
@@ -40,15 +55,18 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, flatten: (Vector2D, Vector2D) => Double, rightVec0: Vector2D, rightVec1: Vector2D): Double =
+        flatten(leftVec0, leftVec1) + flatten(rightVec0, rightVec1)
+
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
+
 
 
 
@@ -71,6 +89,7 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] =
+        ballsArray.mapValues({ case (r, d) => (4 / 3) * Math.PI * Math.pow(r, 3) * d }).toSeq.sortBy(e => e._2).map(e => e._1)
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,12 +1,75 @@
+import Exercises.Vector2D
 import utest._
 
 object Test extends TestSuite{
 
-    val tests = Tests{
+    val tests: Tests = Tests{
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+        }
+
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(0, 0) == 0)
+            assert(Exercises.sumOfDivBy3Or5(0, 4) == 3)
+            assert(Exercises.sumOfDivBy3Or5(0, 5) == 8)
+            assert(Exercises.sumOfDivBy3Or5(-4, 0) == -3)
+            assert(Exercises.sumOfDivBy3Or5(-5, 3) == -5)
+            assert(Exercises.sumOfDivBy3Or5(-5, 10) == 25)
+        }
+
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(0) == Seq())
+            assert(Exercises.primeFactor(1) == Seq())
+            assert(Exercises.primeFactor(-1) == Seq())
+            assert(Exercises.primeFactor(3) == Seq(3))
+            assert(Exercises.primeFactor(7) == Seq(7))
+            assert(Exercises.primeFactor(10) == Seq(2, 5))
+            assert(Exercises.primeFactor(15) == Seq(3, 5))
+            assert(Exercises.primeFactor(-15) == Seq(3, 5))
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(-80) == Seq(2, 5))
+        }
+
+        'test_sumScalars - {
+            assert(Exercises.sumScalars(Vector2D(0, 1), Vector2D(2, 3), Vector2D(4, 5), Vector2D(6, 7)) == 62)
+            assert(Exercises.sumScalars(Vector2D(8, 26), Vector2D(0, -10), Vector2D(8, 1), Vector2D(1337, -117)) == 10319)
+        }
+
+        'test_sumCosines - {
+            assert(Exercises.sumCosines(Vector2D(1, 2), Vector2D(3, 4), Vector2D(-5, -6), Vector2D(7, 8)) < -0.015)
+        }
+
+
+        'test_sortByHeavyweight - {
+            assert(
+                Exercises.sortByHeavyweight() == Seq(
+                    "Tin",
+                    "Platinum",
+                    "Nickel",
+                    "Aluminum",
+                    "Titanium",
+                    "Lead",
+                    "Sodium",
+                    "Uranium",
+                    "Gold",
+                    "Tungsten",
+                    "Zirconium",
+                    "Chrome",
+                    "Iron",
+                    "Copper",
+                    "Silver",
+                    "Plutonium",
+                    "Cobalt",
+                    "Cesium",
+                    "Calcium",
+                    "Lithium",
+                    "Magnesium",
+                    "Potassium",
+                    "Graphite",
+                )
+            )
         }
     }
 }
