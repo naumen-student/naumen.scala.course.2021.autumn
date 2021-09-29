c8a70531cab67bad8a20727c0639c7504fec99f2
lev <levtrifonov@mail.ru>
2021-09-29 21:53:53 +0500

    Завершил первое задание и тесты к нему

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -12,21 +12,44 @@ object Exercises {
      * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
      * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
      */
-    def prettyBooleanFormatter1(x: Any): String = ???
+    def prettyBooleanFormatter1(x: Any): String = x match {
+        case true => "правда"
+        case false => "ложь"
+        case _ => x.toString
+    }
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+    def prettyBooleanFormatter2(x: Any): String = {
+        val classX = x.getClass.getName
+        if (classX == "java.lang.Boolean"){
+            if (x == true)
+                "правда"
+            else
+                "ложь"
+        }
+        else
+            x.toString
+    }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+    def prettyBooleanFormatter3(x: Any): String = {
+        if (x.isInstanceOf[Boolean]){
+            if (x == true)
+                "правда"
+            else
+                "ложь"
+        }
+        else
+            x.toString
+    }
 
 
     /**
      * Задание №2
      * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
      *
-     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
+     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции.
      * Обратите внимание на возвращаемые типы.
      */
-    def max1(xs: Seq[Int]): Int = ???
+    def max1(xs: Seq[Int]): Int = if (xs.isEmpty) 0 else xs.max
 
     def max2(xs: Seq[Int]): Seq[Int] = ???
 
@@ -41,7 +64,7 @@ object Exercises {
     /**
      * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
      * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
-     */ 
+     */
     def sum1(x: Int, y: Int): Int = sumIntegers(???)
     def sum2(x: Int, y: Int): Int = sumIntegers(???)
     def sum3(x: Int, y: Int): Int = sumIntegers(???)
--- homeworks/homework_3/src/test/scala/homework_3/Test.scala
+++ homeworks/homework_3/src/test/scala/homework_3/Test.scala
@@ -4,10 +4,28 @@ import utest._
 
 object Test extends TestSuite{
 
+
     val tests = Tests{
         'test_example - {
             val trueStr = "правда"
             assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+        }
+        'prettyBooleanFormatter1 - {
+            val trueStr = "правда"
+            assert(Exercises.prettyBooleanFormatter1(true) == trueStr)
+            val falseStr = "ложь"
+            assert(Exercises.prettyBooleanFormatter1(false) == falseStr)
+            val numStr = "123"
+            assert(Exercises.prettyBooleanFormatter1(123) == numStr)
+        }
+
+        'prettyBooleanFormatter2 - {
+            val trueStr = "правда"
+            assert(Exercises.prettyBooleanFormatter2(true) == trueStr)
+            val falseStr = "ложь"
+            assert(Exercises.prettyBooleanFormatter2(false) == falseStr)
+            val numStr = "123"
+            assert(Exercises.prettyBooleanFormatter2(123) == numStr)
         }
     }
 }
