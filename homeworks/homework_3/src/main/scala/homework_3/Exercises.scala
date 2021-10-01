8e5b3cc7a5cae6e691c82bc6f7d1336f57b4ef01
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-10-01 23:18:12 +0500

    resolve homework_3

--- homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
+++ homeworks/homework_3/src/main/scala/homework_3/Exercises.scala
@@ -3,47 +3,70 @@ package homework_3
 object Exercises {
 
 
-    /**
-     * Задание №1
-     * Реализуйте функцию, которая принимает любой тип и преобразует его в строку.
-     * Для всех типов кроме Boolean достаточно воспользоваться стандартной функцией .toString.
-     * Для типа Boolean сделайте особое преобразование: true -> "правда", false -> "ложь".
-     *
-     * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе. 
-     * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
-     */
-    def prettyBooleanFormatter1(x: Any): String = ???
+  /**
+   * Задание №1
+   * Реализуйте функцию, которая принимает любой тип и преобразует его в строку.
+   * Для всех типов кроме Boolean достаточно воспользоваться стандартной функцией .toString.
+   * Для типа Boolean сделайте особое преобразование: true -> "правда", false -> "ложь".
+   *
+   * Реализуйте функцию тремя разными способами, отличающимися тем, как определяется какой тип имеет значение переданное в аргументе.
+   * Определение типа необходимо для реализации специальной логики работы с Boolean значениями, которая описана в условии выше.
+   */
+  def prettyBooleanFormatter1(x: Any): String = x match {
+    case bool: Boolean => if (bool) "правда" else "ложь"
+    case _ => x.toString
+  }
 
-    def prettyBooleanFormatter2(x: Any): String = ???
+  def prettyBooleanFormatter2(x: Any): String = {
+    if (x.isInstanceOf[Boolean]) {
+      if (x == true) "правда" else "ложь"
+    }
+    else {
+      x.toString
+    }
+  }
 
-    def prettyBooleanFormatter3(x: Any): String = ???
+  def prettyBooleanFormatter3(x: Any): String = {
+    val className = x.getClass.getSimpleName.toLowerCase()
+    val booleanClassName = true.getClass.getSimpleName
+
+    if (className == booleanClassName)
+      if (x.asInstanceOf[Boolean]) "правда" else "ложь"
+    else
+      x.toString
+  }
 
 
-    /**
-     * Задание №2
-     * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
-     *
-     * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции. 
-     * Обратите внимание на возвращаемые типы.
-     */
-    def max1(xs: Seq[Int]): Int = ???
+  /**
+   * Задание №2
+   * Реализуйте функцию нахождения максимального числа в переданной коллекции интов (можно использовать все методы стандартной библиотеки).
+   *
+   * Реализуйте функцию тремя разными способами, отличающимися тем как функция себя ведет на пустой коллекции.
+   * Обратите внимание на возвращаемые типы.
+   */
+  def max1(xs: Seq[Int]): Int = if (xs.nonEmpty) xs.max else Int.MinValue
 
-    def max2(xs: Seq[Int]): Seq[Int] = ???
+  def max2(xs: Seq[Int]): Seq[Int] = xs.reduceOption(_ max _).toSeq
 
-    def max3(xs: Seq[Int]): Option[Int] = ???
+  def max3(xs: Seq[Int]): Option[Int] = xs.reduceOption(_ max _)
 
-    /**
-     * Задание №3
-     * Допустим дана функция sumIntegers, которая умеет суммировать числа.
-     */
-    def sumIntegers[CollectionType <: Iterable[Int]](xs: CollectionType): Int = xs.sum
+  case class MyPair(first: Int, second: Int) extends Iterable[Int] {
+    override def iterator: Iterator[Int] = Seq(first, second).toIterator
+  }
 
-    /**
-     * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
-     * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
-     */ 
-    def sum1(x: Int, y: Int): Int = sumIntegers(???)
-    def sum2(x: Int, y: Int): Int = sumIntegers(???)
-    def sum3(x: Int, y: Int): Int = sumIntegers(???)
+  /**
+   * Задание №3
+   * Допустим дана функция sumIntegers, которая умеет суммировать числа.
+   */
+  def sumIntegers[CollectionType <: Iterable[Int]](xs: CollectionType): Int = xs.sum
 
+  /**
+   * Реализуйте на основе нее 3 варианта суммирования 2х чисел, отличающиеся способом передачи этих 2х чисел в функцию sumIntegers.
+   * Как минимум одна из реализаций должна использовать тип данных (класс) написанный вами самостоятельно.
+   */
+  def sum1(x: Int, y: Int): Int = sumIntegers(List(x, y))
+
+  def sum2(x: Int, y: Int): Int = sumIntegers(Seq(x, y))
+
+  def sum3(x: Int, y: Int): Int = sumIntegers(MyPair(x, y))
 }
