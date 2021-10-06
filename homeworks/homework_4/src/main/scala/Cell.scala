128879da468c986e3747c19800d00000d3249022
lev <levtrifonov@mail.ru>
2021-10-06 16:54:41 +0500

    test_referenceCell

--- homeworks/homework_4/src/main/scala/Cell.scala
+++ homeworks/homework_4/src/main/scala/Cell.scala
@@ -2,14 +2,27 @@ trait Cell {
   def toString(): String
 }
 
-class EmptyCell extends Cell{
+class EmptyCell extends Cell {
   override def toString: String = "empty"
 }
 
-class NumberCell(val value: Int) extends Cell{
+class NumberCell(val value: Int) extends Cell {
   override def toString: String = value.toString
 }
 
-class StringCell(val value: String) extends Cell{
+class StringCell(val value: String) extends Cell {
   override def toString: String = value.toString
+}
+
+class ReferenceCell(val row: Int, val column: Int, val table: Table) extends Cell {
+  override def toString: String = {
+    val cell = table.getCell(row, column)
+    cell match {
+      case Some(r: ReferenceCell) =>
+        if (r.table.getCell(r.row, r.column) == Option(this)) "cyclic"
+        else r.toString
+      case None => "outOfRange"
+      case Some(c) => c.toString
+    }
+  }
 }
