d98a55da725b029404a0fd720f35838ed6d2f726
Aydartsyan_Grisha <57267232+AydartsyanGrisha@users.noreply.github.com>
2021-10-31 00:33:11 +0500

    Tasks done

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,13 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    val result = collection.mutable.MutableList[T]()
+    for (i <- Range(seq.length - 1, -1, -1)) {
+      result += seq(i)
+    }
+    result
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,19 +15,58 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    fibonacci(idx).last
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    val result = collection.mutable.MutableList[Int](0, 1)
+    for (i <- Range(2, idx + 1)) {
+      result += result(i - 2) + result(i - 1)
+    }
+    result
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
-                       "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
-                       "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
-                       "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
-                       "Y" -> "-.--", "Z" -> "--..")
+    "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
+    "M" -> "--", "N" -> "-.", "O" -> "---", "P" -> ".--.", "Q" -> "--.-", "R" -> ".-.",
+    "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
+    "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = {
+    val processedText = text.toUpperCase
+    val result = collection.mutable.MutableList[String]()
+    for (symbol <- processedText) {
+      if(symbol.isLetter) {
+        result += MORSE(symbol.toString)
+      } else {
+        result += symbol.toString
+      }
+    }
+    result.mkString(" ")
+  }
 
 
-  def wordReverse(text: String): String = ???
-
+  def wordReverse(text: String): String = {
+    var start = 0
+    var end = 0
+    var result = ""
+    for (symbol <- text) {
+      if (symbol.isLetter) {
+        end += 1
+      } else {
+        val word = text.substring(start, end)
+        val reversed = word.reverse
+        if (word.nonEmpty && word.head.isUpper) {
+          result += reversed.toLowerCase().capitalize
+        } else {
+          result += reversed
+        }
+        result += symbol.toString
+        end += 1
+        start = end
+      }
+    }
+    result
+  }
 }
