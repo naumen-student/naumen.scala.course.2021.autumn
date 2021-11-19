d6334628e24c1a54911eef35613399171f422f92
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2021-11-19 19:09:59 +0300

    8 homework

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,37 @@
+import scala.util.matching.Regex
 import scala.util.Try
+
 object Exercises {
+  val Err = "Error"
+
+  trait Read[T]{
+    def read(a: String): Either[String, T]
+  }
+
+  object Read{
+    def read[T](s: String)(implicit read: Read[T]): Either[String, T] = read.read(s)
+  }
+
+  implicit class ReadOps(s: String){
+    def read[T]()(implicit read: Read[T]): Either[String, T] = read.read(s)
+  }
+
+  implicit val StringRead: Read[String] = (s: String) => Right(s)
+
+  implicit val intRead: Read[Int] = (str: String) => Try(str.toInt).toOption match {
+    case None => Left(Err)
+    case Some(x) => Right(x)
+  }
+
+  val somePattern: Regex = """Some\((\S+)\)""".r
+
+  implicit def ReadOption[T: Read]: Read[Option[T]] = {
+    case somePattern(value) => value.read[T] match {
+      case Right(some) => Right(Some(some))
+      case Left(_) => Left(Err)
+    }
+    case a => if (a == "None") Right(None) else Left(Err)
+  }
+
+  def CheckIfNumber(string: String): Boolean = string forall Character.isDigit
 }
