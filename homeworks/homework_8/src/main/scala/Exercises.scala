288b85d930752b470bb38bf557f4db52c54c56a1
imedvedev@embedika.ru <c6r3s5W9>
2021-11-20 15:26:02 +0500

    homework_8

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,29 @@
+import scala.language.higherKinds
+import scala.util.matching.Regex
 import scala.util.Try
+
 object Exercises {
+
+  trait Readable[T] {
+    def read(v: String): Either[String, T]
+  }
+
+  implicit class Read(value: String) {
+    def read[V: Readable]: Either[String, V] = implicitly[Readable[V]].read(value)
+  }
+
+  object Read {
+    def read[V: Readable](s: String): Either[String, V] = implicitly[Readable[V]].read(s)
+  }
+
+  implicit val stringReadable: Readable[String] = s => Right(s)
+  implicit val intReadable: Readable[Int] = i => Try(i.toInt).toEither.left.map(_ => i)
+
+  val someRegex: Regex = "Some\\((.*)\\)".r
+
+  implicit def optReadable[T: Readable]: Readable[Option[T]] = {
+    case "None" => Right(None)
+    case s @ someRegex(a) => implicitly[Readable[T]].read(a).map(Some(_)).left.map(_ => s)
+    case s => Left(s)
+  }
 }
