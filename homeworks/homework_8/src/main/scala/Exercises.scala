c29c9c8b24d5705964e10ae68bb037872e11bf0d
EliteHacker228 <34379703+EliteHacker228@users.noreply.github.com>
2021-11-25 18:02:45 +0500

    Some semantic changes

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -7,28 +7,28 @@ object Exercises {
     def read[A](value: String)(implicit reader: Read[A]): Either[String, A] = reader.read(value)
   }
 
-  implicit class ReadOperations(value: String) {
+  implicit class ReadImpl(value: String) {
     def read[A](implicit reader: Read[A]): Either[String, A] = reader.read(value)
   }
 
   implicit def StringRead: Read[String] = (value: String) => Right(value)
 
   implicit def IntRead: Read[Int] = (value: String) => {
-    if (IsStringIntable(value)) {
+    if (IsNumeric(value))
       Right(value.toInt)
-    } else {
+    else
       Left(value)
     }
   }
 
-  def IsStringIntable(value: String) = value.forall(_.isDigit);
+  def IsNumeric(value: String) = value.forall(_.isDigit);
 
   implicit def OptionRead[A: Read]: Read[Option[A]] = {
-    case value if IsStringContainsSome(value) && getSomesContent(value).read[A].isRight => Right(Some(getSomesContent(value).read[A].right.get))
+    case value if IsStringContainsSome(value) && getSomeBracesContent(value).read[A].isRight => Right(Some(getSomeBracesContent(value).read[A].right.get))
     case "None" => Right(None)
     case _ => Left("")
   }
 
   def IsStringContainsSome(value: String) = value.startsWith("Some(") && value.endsWith(")");
-  def getSomesContent(value: String) = value.substring("Some(".length, value.length-1)
+  def getSomeBracesContent(value: String) = value.substring("Some(".length, value.length-1)
 }
