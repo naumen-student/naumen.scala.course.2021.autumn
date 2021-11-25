770f2f9cbd3a97492ec27a5f8c9a0dfbb5857572
EliteHacker228 <34379703+EliteHacker228@users.noreply.github.com>
2021-11-25 17:55:16 +0500

    Update Exercises.scala

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -11,7 +11,6 @@ object Exercises {
     def read[A](implicit reader: Read[A]): Either[String, A] = reader.read(value)
   }
 
-  
   implicit def StringRead: Read[String] = (value: String) => Right(value)
 
   implicit def IntRead: Read[Int] = (value: String) => {
